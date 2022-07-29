bb12370fcb77ec0fc1630c85f151fb6386826656
liyunhui <questioner0620@gmail.com>
2022-07-29 14:28:23 +0800

    this is second time

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 console.log(123)
-const addButton = document.getElementById('add')
-const subButton = document.getElementById('sub')
+const addButton = document.getElementById("add")
+const subButton = document.getElementById("sub")
 addButton.addEventListener('click', control,false)
 subButton.addEventListener('click', control,false)
 function control(e){
     const temp = e.currentTarget.id
     const font =document.getElementById('p')
-    if(temp === 'add'){
+    if(temp === "add"){
         font.style.fontSize = tempSize*1.05 +'px'
     }else{
         font.style.fontSize = tempSize*0.95 +'px'
